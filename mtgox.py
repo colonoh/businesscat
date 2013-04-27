294994cb39e4f41021a7afb012771196a811956a
Steve <steve.j.warner@gmail.com>
2013-04-27 19:43:31 -0300

    Update mtgox.py

--- mtgox.py
+++ mtgox.py
@@ -1,15 +1,21 @@
-import hmac, base64, hashlib, urllib.request, urllib.error, urllib.parse
+import hmac, base64, hashlib, urllib.request
 import time
 
 base = 'https://data.mtgox.com/api/2/'
 
-def create_nonce(self):
+def create_nonce():
     return int(time.time() * 1000000)
 
 def makereq(key, secret, path, data):
+    print(path + chr(0) + data)
     hash_data = (path + chr(0) + data).encode() # turn it a byte representation instead of string
+    print((path + chr(0) + data).encode())
     #secret = base64.b64decode(secret)
-    secret = base64.b64decode(bytes(secret, "UTF-8"))
+    
+    print(secret)
+    a = bytes(secret, "UTF-8")
+    print(a)
+    secret = base64.b64decode(a)
     sha512 = hashlib.sha512
     hmac = str(hmac.new(secret, hash_data, sha512))
 
