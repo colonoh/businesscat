fcdb8243ad1ed9054a8961abd0028696c710da65
colonoh <steve.j.warner@gmail.com>
2013-04-27 23:49:23 +0100

    fixed b64 padding problem

--- mtgox.py
+++ mtgox.py
@@ -7,15 +7,8 @@ def create_nonce():
     return int(time.time() * 1000000)
 
 def makereq(key, secret, path, data):
-    print(path + chr(0) + data)
     hash_data = (path + chr(0) + data).encode() # turn it a byte representation instead of string
-    print((path + chr(0) + data).encode())
-    #secret = base64.b64decode(secret)
-    
-    print(secret)
-    a = bytes(secret, "UTF-8")
-    print(a)
-    secret = base64.b64decode(a)
+    secret = base64.b64decode(secret.encode())
     sha512 = hashlib.sha512
     hmac = str(hmac.new(secret, hash_data, sha512))
 
