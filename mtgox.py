6ff238fc1646334db3d1c90494bcdd5bac3a99a5
Steve <steve.j.warner@gmail.com>
2013-04-27 19:24:20 -0300

    added nonce func and encoded a string

--- mtgox.py
+++ mtgox.py
@@ -1,8 +1,13 @@
 import hmac, base64, hashlib, urllib.request, urllib.error, urllib.parse
+import time
+
 base = 'https://data.mtgox.com/api/2/'
 
+def _create_nonce(self):
+    return int(time.time() * 1000000)
+
 def makereq(key, secret, path, data):
-    hash_data = path + chr(0) + data
+    hash_data = (path + chr(0) + data).encode() # turn it a byte representation instead of string
     secret = base64.b64decode(secret)
     sha512 = hashlib.sha512
     hmac = str(hmac.new(secret, hash_data, sha512))
