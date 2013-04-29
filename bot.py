f1a174d6b9806c7937797b421246286dae3f57ff
Steve <steve.j.warner@gmail.com>
2013-04-29 20:11:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,28 @@
 buy low, sell high
 '''
 from mtgox import send_request
+import time
 
 initial_target = '135' # USD/BTC
-initial_action = 'buy'
 
-# check the trend
-# adjust the target value depending on trend
-# if below/above the target value, buy/sell
-# set new target and switch to opposite action
+buying = True # start with buying
 
+# every X minutes...
+    
+    ## check the trend and adjust the target value accordingly
+    
+    # if below the adjusted target value, buy
+    if buying and current_price < buy_price:
+        buy()
+        # pick new target and switch to selling
+        
+    # if above the dajusted target value, sell
+    elif not buying and current_price > sell_price:
+        sell()
+        # pick new target and switch to buying
+    
+    # if neither, do nothing?
+    
+    time.sleep(60) # in any case, sleep for X minutes
 
 
