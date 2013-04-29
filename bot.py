a134ec4446de26cd43247571ce6683874a7be0b6
Steve <steve.j.warner@gmail.com>
2013-04-29 18:45:59 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+'''
+buy low, sell high
+'''
+from mtgox import send_request
+
+initial_target = '135' # USD/BTC
+initial_action = 'buy'
+
+# check the trend
+# adjust the target value depending on trend
+# if below/above the target value, buy/sell
+# set new target and switch to opposite action
+
+
+
