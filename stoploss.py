cf1de9050afb8caa9e5dfd2a6b6a3a9a6e248114
Steve <steve.j.warner@gmail.com>
2013-04-30 19:39:55 -0300

    removed buying code

--- stoploss.py
+++ stoploss.py
@@ -5,35 +5,19 @@ from mtgox import send_request
 import time
 
 stoploss_percentage = 0.10
-#buy_percentage = 0.05
 high_price = 150
 
 # every X minutes...
 while(True):
     # check the current price, set high price if needed
-    buy_price, sell_price = mtgox.get_price()
-    high_price = max(buy_price, high_price)
+    buy_price, sell_price = mtgox.get_prices()
+    high_price = max(sell_price, high_price)
+    print('Buy=', buy_price, 'Sell=', sell_price, 'High=', high_price)
     
     # check to se if price is falling
     
     # if under a certain amount, sell
-    if buy_price < high_price * (1 - stoploss_percentage):
-        mtgox.sell()
-        # special code to handle buying after a crash
-       
-    # if not stop-selling, have a floating buy order X% under the current price
-    # BUT we want to catch the immediate drop too! not just buy in at the top on the way down
-    #if not selling:
-        # see if buy orders exists
-        
-        # cancel last buy order and put new buy order in at correct price, if needed
-        # new buy should be at least X% under high_price!
-        #if:
-            #cancel()
-            #place_buy()
-    
-    #
-    if selling:
-        time.sleep(20)
-    else:
-        time.sleep(120)
+    if sell_price < high_price * (1 - stoploss_percentage):
+        mtgox.place_sell()
+
+    time.sleep(20)
