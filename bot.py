161b2445136d681db15badb3c03116efc39af142
Steve <steve.j.warner@gmail.com>
2013-05-09 21:36:01 -0300

    Major update of bot code
    - using all mtgox values now, converting to human-readable only for printing
    - switching buying with action = {buying,selling} variable
    - updated print statements to be more friendly
    - created function which returns time

--- bot.py
+++ bot.py
@@ -4,36 +4,46 @@ buy low, sell high
 import mtgox
 import time
 
-target_price = '135' # USD/BTC
-percent_change = 0.03
-buying = True # start with buying
 USD = 1e8 # conversion factor for USD
 BTC = 1e5 # conversion factor for BTC
+
+# ==================================================================
+# ==EDIT THESE BELOW================================================
+# ==================================================================
+target_price = 126.381 # USD/BTC
+percent_change = 0.03 # percent change at which to do the opposite action
+action = 'buying' # start with buying
+amount = 0.01 # amount of BTC to play with
+# ==================================================================
+
+# all numbers are mtgox friendly
+target_price = target_price*USD
+amount = amount*BTC
+
+# return the current time nicely formatted
+def now():
+    return time.strftime(%I:%M:%S %p)
 
 # every X minutes...
 while(True):
     ## check the trend and adjust the target value accordingly
     buy_price, sell_price = mtgox.get_prices()
-    print(time.strftime(%I:%M:%S %p), 'Bid:', buy_price/USD, 'Ask:', sell_price/USD, 'Buying:', buying, 'Target:', target_price/USD)
+    print '[{}]    Bid: {} $/BTC    Ask: {} $/BTC    Target: {} $/USD    Currently {}'.format(now(), buy_price/USD, sell_price/USD, target_price/USD, action)
     
     # if below the adjusted target value, buy
-    if buying and sell_price =< target_price:
-        print(time.strftime(%I:%M:%S %p), 'Buying at', target_price/USD)
+    if action == 'buying' and sell_price =< target_price:
+        print '[{}]    Buying {} BTC at {} $/BTC'.format(now(), amount/BTC, target_price/USD)
         order_id = mtgox.order('bid', amount, target_price)
         # pick new target and switch to selling
-        buying = False
-        target_price = target_price*(1+percent_change)
+        action = 'selling'
+        target_price = target_price*(1 + percent_change)
         
-    # if above the dajusted target value, sell
-    elif not buying and buy_price >= target_price:
+    # if above the adjusted target value, sell
+    elif action == 'selling' and buy_price >= target_price:
+        print '[{}]    Selling {} BTC at {} $/BTC'.format(now(), amount/BTC, target_price/USD)
         order_id = mtgox.order('ask', amount, target_price)
-        print(time.strftime(%I:%M:%S %p), 'Selling at', target_price/USD)
         # pick new target and switch to buying
-        buying = True
-        target_price = target_price*(1-percent_change)
-    
-    # if neither, do nothing?
+        action = 'buying'
+        target_price = target_price*(1 - percent_change)
     
     time.sleep(60) # in any case, sleep for X minutes
-
-
