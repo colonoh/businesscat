2f0d8d0b5a2abd8cb1a1746c5f51c8aee8a6b132
Steve <steve.j.warner@gmail.com>
2013-05-09 23:49:43 -0300

    fixed minor typos

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ buy_price, sell_price = mtgox.get_prices()
 print('Last buy:', buy_price, 'Last sell:', sell_price)
 
 # place an order
-order_id = mtgox.order('buy', .01*1e8, 1*1e5)
+order_id = mtgox.order('bid', .01*1e8, 1*1e5)
 print('Order id:', order_id)
 
 # cancel that last order!
 method_args = {'oid': order_id} 
-output = mtgox.send_request('BTCUSD/MONEY/ORDER/CANCEL', method_args)
+output = mtgox._send_request('BTCUSD/MONEY/ORDER/CANCEL', method_args)
 pprint.pprint(output)
