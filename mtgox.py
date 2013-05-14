450bcb6a3857732a0ac19cab83718c179f69d337
Steve <steve.j.warner@gmail.com>
2013-05-14 14:22:54 -0300

    docstrings

--- mtgox.py
+++ mtgox.py
@@ -1,15 +1,18 @@
+'''Interface to MtGox'''
 import hmac, urllib, urllib2, hashlib, base64, json, sys, time
-from key import key, secret
+
+import key
     
 # Send a request like "BTCUSD/money/ticker" to the server and return the output in JSON format
 def _send_request(api_method, method_args={}):
+    '''Sends a request to MtGox and returns the full output'''
     method_args['nonce'] = int(time.time() * 1000000) # add the nonce to the list of arguments
     method_args = urllib.urlencode(method_args.items()) # convert from a dictionary to a "percent-encoded" string
     
-    auth_code = hmac.new(base64.b64decode(secret), api_method + chr(0) + method_args, hashlib.sha512)
+    auth_code = hmac.new(base64.b64decode(key.secret), api_method + chr(0) + method_args, hashlib.sha512)
     header = {
         'User-Agent': 'businesscat-bot',
-        'Rest-Key': key,
+        'Rest-Key': key.key,
         'Rest-Sign': base64.b64encode(str(auth_code.digest())),
     }
 
@@ -26,14 +29,14 @@ def _send_request(api_method, method_args={}):
         sys.exit('Error: MtGox request was unsuccessful.')
 
 
-# get current prices for buy and sell orders
 def get_prices():
+    '''return the latest bid and ask prices'''
     output = _send_request('BTCUSD/MONEY/TICKER_FAST')    
     return int(output['data']['buy']['value_int']), int(output['data']['sell']['value_int'])
 
 
-# buy (or sell) some bitcoins, returns the order ID
 def order(order_type, amount, price = None):
+    '''send in an order and return the order ID'''
     args = {'amount_int': amount, 'type': order_type}
     
     # if there is no price argument, place a market order
@@ -41,8 +44,9 @@ def order(order_type, amount, price = None):
         args['price_int'] = price
     
     output = _send_request('BTCUSD/MONEY/ORDER/ADD', args)
-    return str(output['data']) # return the order ID
+    return str(output['data'])
 
 
 def cancel(order_id):
+    '''takes an order ID and requests that it be canceled'''
     return _send_request('BTCUSD/MONEY/ORDER/CANCEL', {'oid': order_id})
